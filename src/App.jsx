795b7e8e1f4ce12410febf5aedb0b6e1b13b338e
Priyam Bhattacharya <itspriyambhattacharya@gmail.com>
2025-06-08 13:01:31 +0530

    Add content to App component with a test div for layout

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,11 @@ import "./App.css";
 function App() {
   const [color, setColor] = useState("olive");
   return (
-    <div className="w-full h-screen" style={{ backgroundColor: color }}></div>
+    <div className="w-full h-screen" style={{ backgroundColor: color }}>
+      <div className="flex flex-wrap justify-center fixed bottom-12 inset-x-0 border-2 border-amber-700 border-solid">
+        test
+      </div>
+    </div>
   );
 }
 
