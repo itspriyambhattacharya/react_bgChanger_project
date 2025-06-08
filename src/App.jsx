eac3050ace7fa01f4c7a30da93468d04b00af4c7
Priyam Bhattacharya <itspriyambhattacharya@gmail.com>
2025-06-08 13:11:27 +0530

    Refactor App component to replace test div with color selection buttons

--- src/App.jsx
+++ src/App.jsx
@@ -5,8 +5,25 @@ function App() {
   const [color, setColor] = useState("olive");
   return (
     <div className="w-full h-screen" style={{ backgroundColor: color }}>
-      <div className="flex flex-wrap justify-center fixed bottom-12 inset-x-0 border-2 border-amber-700 border-solid">
-        test
+      <div className="flex flex-wrap justify-center gap-5 fixed bottom-12 inset-x-0 py-6 bg-white">
+        <button className="cursor-pointer bg-red-700 px-4 py-2 w-[90px] rounded-4xl text-white">
+          Red
+        </button>
+        <button className="cursor-pointer bg-blue-700 px-4 py-2 w-[90px] rounded-4xl text-white">
+          Blue
+        </button>
+        <button className="cursor-pointer bg-green-700 px-4 py-2 w-[90px] rounded-4xl text-white">
+          Green
+        </button>
+        <button className="cursor-pointer bg-yellow-700 px-4 py-2 w-[90px] rounded-4xl text-white">
+          Yellow
+        </button>
+        <button className="cursor-pointer bg-pink-400 px-4 py-2 w-[90px] rounded-4xl text-white">
+          Pink
+        </button>
+        <button className="cursor-pointer bg-black px-4 py-2 w-[90px] rounded-4xl text-white">
+          Black
+        </button>
       </div>
     </div>
   );
