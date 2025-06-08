384aad35bc2af9ad1a8964bdc18a1644afd8c9a6
Priyam Bhattacharya <itspriyambhattacharya@gmail.com>
2025-06-08 13:22:15 +0530

    Refactor button components in App to use dedicated click handlers for color selection

--- src/App.jsx
+++ src/App.jsx
@@ -3,26 +3,55 @@ import "./App.css";
 
 function App() {
   const [color, setColor] = useState("olive");
+  function addRed() {
+    setColor("red"); // not a good way to add color
+  }
   return (
     <>
       <div className="w-full h-screen" style={{ backgroundColor: color }}>
         <div className="flex flex-wrap justify-center gap-5 fixed bottom-12 inset-x-0 py-6 bg-white">
-          <button className="cursor-pointer bg-red-700 px-4 py-2 w-[90px] rounded-4xl text-white">
+          <button
+            onClick={addRed}
+            className="cursor-pointer bg-red-700 px-4 py-2 w-[90px] rounded-4xl text-white"
+          >
             Red
           </button>
-          <button className="cursor-pointer bg-blue-700 px-4 py-2 w-[90px] rounded-4xl text-white">
+          <button
+            onClick={() => {
+              setColor("blue");
+            }}
+            className="cursor-pointer bg-blue-700 px-4 py-2 w-[90px] rounded-4xl text-white"
+          >
             Blue
           </button>
-          <button className="cursor-pointer bg-green-700 px-4 py-2 w-[90px] rounded-4xl text-white">
+          <button
+            onClick={() => {
+              setColor("green");
+            }}
+            className="cursor-pointer bg-green-700 px-4 py-2 w-[90px] rounded-4xl text-white"
+          >
             Green
           </button>
-          <button className="cursor-pointer bg-yellow-700 px-4 py-2 w-[90px] rounded-4xl text-white">
+          <button
+            onClick={() => {
+              setColor("yellow");
+            }}
+            className="cursor-pointer bg-yellow-700 px-4 py-2 w-[90px] rounded-4xl text-white"
+          >
             Yellow
           </button>
-          <button className="cursor-pointer bg-pink-400 px-4 py-2 w-[90px] rounded-4xl text-white">
+          <button
+            onClick={() => setColor("pink")}
+            className="cursor-pointer bg-pink-400 px-4 py-2 w-[90px] rounded-4xl text-white"
+          >
             Pink
           </button>
-          <button className="cursor-pointer bg-black px-4 py-2 w-[90px] rounded-4xl text-white">
+          <button
+            onClick={() => {
+              setColor("black");
+            }}
+            className="cursor-pointer bg-black px-4 py-2 w-[90px] rounded-4xl text-white"
+          >
             Black
           </button>
         </div>
